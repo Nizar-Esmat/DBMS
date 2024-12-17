5ca25a2f4422351bffa6dedd80005dbf1999b587
Nizar-Esmat <nizaresmat2000@gmail.com>
2024-12-18 01:33:19 +0200

    make the main select

--- main.sh
+++ main.sh
@@ -1,3 +1,38 @@
 #!/bin/bash 
 
-echo "DBMS in nizar branch"
+# Create a select to choose the option 
+echo "Enter the option you want"
+
+select option in "Create Database" "List Databases" "Delete Database" "Connect to Database" "exit"
+do
+    case $option in
+        "Create Database")
+            echo "You selected Create Database"
+            ;;
+        "List Databases")
+            echo "You selected List Databases"
+            ;;
+        "Delete Database")
+            echo "You selected Delete Database"
+            ;;
+        "Connect to Database")
+            echo "You selected Connect to Database"
+            ;;
+        "exit")
+            read -p "Do you want to exit? (yes/no): " exit
+            if [[  $exit =~ "yes" ]]; then
+                echo "You exited the system"
+                break
+            elif [[ $exit =~ "no" ]]; then
+                echo "You are still in the system, choose an option"
+                continue
+            else
+                echo "Invalid input"
+            fi
+            ;;
+        *)
+            echo "Invalid input. Please try again."
+            ;;
+    esac
+done
+
