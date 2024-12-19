4a42acc265853fcfc609c9c2efe9f137e8b53f85
Tarek Hassanin <hassanin.trk@gmail.com>
2024-12-19 11:13:53 +0200

    last change before merging with master

--- main.sh
+++ main.sh
@@ -1,8 +1,67 @@
+<<<<<<< tarek
 #! /usr/bin/bash 
 
-function validate_name(){
+
+#function to validate the input 
+function validate_name() {
+    local name=$1
+
+    if [[ $name =~ ^[0-9] ]]; then
+        echo "Invalid name: Cannot start with a number."
+        return 0
+    fi
+
+    if [[ $name =~ [^A-Za-z0-9_] ]]; then
+        echo "Invalid name: Cannot contain special characters."
+        return 0
+    fi
+
+    return 1 
+}
 
 
+
+
+
+#function to create data base
+clear
+function create_DB(){
+    read -p "Enter the name of the database: " DB_name
+
+    validateName $DB_name
+    validation_result=$?
+    if (($validation_result)); then 
+        if [[ ! -d "$DB_name" ]]; then 
+            mkdir $DB_name
+            echo "Database created."
+        else 
+            echo "This database already exists."
+        fi
+    else
+        echo "You have an error."
+    fi
+}		
+
+
+#function to rename an existing Database
+function rename_DB() {
+	
+	#TO IMPLEMENT ---> Tarek
+
+
+
+}
+
+
+function list_DB() {
+    # Loop over the directory names (Databases) to remove the last character (/)
+    count=0
+    for dir in `ls -d */`
+    do
+        echo "${dir%/}"
+        count=$(($count + 1))
+    done
+    echo "The number of databases is $count."
 }
 
 
@@ -69,11 +128,40 @@ function connect_to_DB(){
 
 
 
-#function to rename an existing Database
-function rename_DB() {
-	
-	#TO IMPLEMENT ---> Tarek
 
+echo "Enter the option you want"
 
+select option in "Create Database" "List Databases" "Delete Database" "Connect to Database" "Exit"
+do
+    case $option in
+        "Create Database")
+            createDB
+            ;;
+        "List Databases")
+            listDB
+            ;;
+        "Delete Database")
+            drop_DB
+            ;;
+        "Connect to Database")
+            connect_to_DB
+            ;;
+        "Exit")
+            read -p "Do you want to exit? (yes/no): " exit
+            if [[  $exit =~ "yes" ]]; then
+                echo "You exited the system."
+                break
+            elif [[ $exit =~ "no" ]]; then
+                echo "You are still in the system. Choose an option."
+                continue
+            else
+                echo "Invalid input."
+            fi
+            ;;
+        *)
+            echo "Invalid input. Please try again."
+            ;;
+    esac
+done
 
-}
+>>>>>>> master
